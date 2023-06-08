035032a636ec37a4764b87573c7fead4955636d2
MallepallySowmya <133375564+MallepallySowmya@users.noreply.github.com>
2023-06-08 17:16:03 +0530

    Update AppiumServerJava.java

--- src/test/java/com/qm/android/utils/AppiumServerJava.java
+++ src/test/java/com/qm/android/utils/AppiumServerJava.java
@@ -30,7 +30,7 @@ public class AppiumServerJava {
         builder = new AppiumServiceBuilder();
         builder.withIPAddress(ConfigReader.getAppiumProp("serverIP"));
         builder.usingPort(Integer.parseInt(ConfigReader.getAppiumProp("port")));
-       builder.usingDriverExecutable(new File("C:/Program Files/nodejs/node.exe"))
+       builder.usingDriverExecutable(new File("C:/Program Files/nodejs/node"))
         builder.withAppiumJS(new File("C:/Users/QM-Testing/AppData/Roaming/npm/node_modules/appium/lib/main.js");
        builder.withCapabilities(cap);
 //        builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
